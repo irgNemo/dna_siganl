f7dd553e0e34ffbed305744bd8ac2481ccd84da2
Israel Roman Godinez <israel.roman@cucei.udg.mx>
2017-11-15 19:57:45 -0600

    cambios

--- consensusAlignment.py
+++ consensusAlignment.py
@@ -9,12 +9,13 @@ from time import time;
 import sys
 import glob, os;
 import shutil;
-
+from Bio import pairwise2;
+from Bio.pairwise2 import format_alignment;
 
 def main():
 	# Configuration parameters
 	platform = sys.platform;
-	foldername = "fastaCOXI";
+	foldername = "SSUrRNA"; #"EF1a";#"COXII"; #"COXI"
 	fileformat = "fasta";
 	consensus_text = "consensus"
 	aligning_format = "fasta"
@@ -22,17 +23,53 @@ def main():
 	output_align_format = ".aln"
 	threshold = 0.7
 	output_dir = "consensus";
-	k = [2,4,6,9,12];
+	clusterNumbers = [6,17,35];
 
-	#crearConsensos(platform, foldername, fileformat, consensus_text, aligning_format, clustalw2_path, output_align_format, threshold, output_dir);
-	unirClusterConsensus(foldername, output_dir, fileformat);
+	crearConsensos(platform, foldername, fileformat, consensus_text, aligning_format, clustalw2_path, output_align_format, threshold, output_dir);
+	unirClusterConsensus(foldername, output_dir, fileformat, clusterNumbers);
+	tablaAlineamiento(foldername, output_dir, fileformat, clusterNumbers, clustalw2_path);
 
-def unirClusterConsensus(foldername, output_dir, fileformat):
-	
-	for filename in glob.glob(foldername + "/" + output_dir + "/*." + fileformat):
-		print (filename);
-	
+def tablaAlineamiento(foldername, output_dir, fileformat, clusterNumbers, clustalw2_path):
+	for k in clusterNumbers:
+		print("Calculando tabla para cluster " + str(k));
+		pattern = ".+K-" + str(k) + ".+";
+		for filename in glob.glob(foldername + "/" + output_dir + "/*500_consensus.fasta"):
+			matchObj = re.match(pattern, filename);
+			if matchObj:	
+				secuencias = parse(filename, fileformat);
+				header = "";
+				contadorCabecera = 0;
+				csv = "";
+				for secuencia1 in secuencias:
+					linea = secuencia1.id;
+					header = ",";
+					for secuencia2 in secuencias:
+						if contadorCabecera == 0:
+							header += secuencia2.id + ",";
+						alignment = pairwise2.align.globalxx(secuencia1,secuencia2);
+						alignmentResult = format_alignment(*alignment[0]);
+						match_score = re.search("Score=(\d+)", alignmentResult);
+						score = match_score.group(1);
+						linea += "," + score;
+					if contadorCabecera == 0:
+						print(header);
+					print(linea);
+					contadorCabecera += 1;
 
+def unirClusterConsensus(foldername, output_dir, fileformat, clusterNumbers):
+	for k in clusterNumbers:
+		pattern = ".+K-"+ str(k) + ".+";
+		records = [];
+		new_filename = None;
+		for filename in glob.glob(foldername + "/" + output_dir + "/*." + fileformat):
+			matchObj = re.match(pattern, filename);
+			if matchObj:
+				record = parse(filename, fileformat);
+				records.append(record[0]);
+				new_filename = re.match('.*(Marker.+500_).*',filename);
+		writeFile(records, foldername + "/" + output_dir + "/" + new_filename.group(1) + "consensus", fileformat);
+		
+		
 def crearConsensos(plataform, foldername, fileformat, consensus_text, aligning_format, clustalw2_path, output_align_format, threshold, output_dir):	
 	if not os.path.exists(foldername + "/" + output_dir):
         	os.makedirs(foldername + "/" + output_dir);
